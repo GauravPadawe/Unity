9a05822c8fa1552f92ea72579a8ac03ff00287b3
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-07 22:49:14 +0530

    Initial commit

---
+++ Unity.py
@@ -0,0 +1,10 @@
+﻿a = [1, 2, 4, 6, 8]
+b = [5, 9, 10, 4, 2, 8]
+
+def unity(a, b):                  # Passing the above list
+    for i in b:                   # for "i" as individual value in b
+        if i not in a:            # if "i" is not present in a
+            a.append(i)           # Modify a
+            
+print (unity(a, b))
+print (a)
